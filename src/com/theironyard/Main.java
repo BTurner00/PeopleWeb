2cbb86296b393908f42d2d6d64482a4deb7adaf2
Ben <bhturne@g.clemson.edu>
2016-06-08 16:49:04 -0400

    Displays 20 per page, etc

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -1,5 +1,10 @@
 package com.theironyard;
 
+import spark.ModelAndView;
+import spark.Session;
+import spark.Spark;
+import spark.template.mustache.MustacheTemplateEngine;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -13,10 +18,66 @@ public class Main {
 
 
         Scanner scanner = new Scanner(System.in);
-
-        HashMap<String, ArrayList<Person>> map = new HashMap<>();
-
         ArrayList<Person> people = parsePerson(SAVE_FILE);
+
+        Spark.init();
+        Spark.get (
+                "/",
+                (request, response) -> {
+                    int next = 0;
+                    int previous= 0;
+                    int offset = 0;
+                    String offsetStr = request.queryParams("offset");
+                    if(offsetStr != null) {
+                        offset = Integer.valueOf(offsetStr);
+                    }
+
+
+                    ArrayList<Person> subset = new ArrayList<>(people.subList(offset, offset+20));
+
+                    if (offset >20) {
+                        previous = offset - 20;
+                    }
+
+                    if ((offset + 20) <= people.size()) {
+                        next = offset+20;
+                    }
+
+                    /*String idStr = request.queryParams("id");
+                    int id = 0;
+                    if (idStr != null) {
+                        id = Integer.valueOf(idStr);
+                    }*/
+
+
+                    HashMap m = new HashMap<>();
+                    m.put("person", subset);
+                    m.put("next", next);
+                    m.put("previous", previous);
+                    m.put("offset", offset);
+                    m.put("nextoption", offset < (people.size() - 20));
+                    m.put("previousoption", offset != 0);
+
+                    return new ModelAndView(m, "home.html");
+                },
+                new MustacheTemplateEngine()
+        );
+
+        Spark.get (
+                "/person",
+                (request, response) -> {
+
+                    String idStr = request.queryParams("id");
+                    int id = Integer.valueOf(idStr);
+                    Person person = people.get(id-1);
+
+                    HashMap m = new HashMap<>();
+                    m.put("person", person);
+
+                    return new ModelAndView(m, "person.html");
+                },
+                new MustacheTemplateEngine()
+        );
 
     }
 
@@ -25,13 +86,17 @@ public class Main {
         ArrayList<Person> people = new ArrayList<>();
         File f = new File(filename);
         Scanner filescanner = new Scanner(f);
+        filescanner.nextLine();
         while (filescanner.hasNext()) {
             String line = filescanner.nextLine();
             String[] columns =line.split("\\,");
             String name = columns[1] + " " + columns[2];
-            Person person  = new Person(name);
+            int id = Integer.valueOf(columns[0]);
+            String email = columns[3];
+            String country = columns[4];
+            String ip = columns[5];
+            Person person  = new Person(name, id, email, country, ip);
             people.add(person);
-
         }
         return people;
     }
--- src/com/theironyard/Person.java
+++ src/com/theironyard/Person.java
@@ -5,8 +5,21 @@ package com.theironyard;
  */
 public class Person {
     String name;
+    int id;
+    String email;
+    String country;
+    String ip;
 
     public Person(String name) {
         this.name = name;
+
+    }
+
+    public Person(String name, int id, String email, String country, String ip) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.country = country;
+        this.ip = ip;
     }
 }
